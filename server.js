68c238940e812f9e659261f2ba4e493408508316
Dhruv <121460129+ishan-arc@users.noreply.github.com>
2024-11-05 20:15:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db.js');
+
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+const app = express();
+
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use('/api/v1/test',require('./routes/testRroute.js'))
+app.use('/api/v1/auth',require('./routes/authRoute.js'))
+app.use('/api/v1/inventory',require('./routes/inventoryRoutes.js'))
+app.use('/api/v1/analytics',require('./routes/analyticsRoutes.js'))
+app.use("/api/v1/admin", require("./routes/adminRoutes.js"));
+
+
+app.get('/', async(req, res) => {
+    res.send("HLLO")
+})
+
+
+const PORT =  8080;
+
+app.listen(PORT, ()=> console.log(`Server running in ${process.env.DEV_MODE} on ${PORT}`));
+
+
+
+
+
+
